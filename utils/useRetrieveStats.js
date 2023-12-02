3dadf61fb032844dbba577b0d78094210ab36df1
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2023-12-03 04:06:03 +1100

    Add `isLoading` to useRetrieveStats hook

--- utils/useRetrieveStats.js
+++ utils/useRetrieveStats.js
@@ -3,15 +3,20 @@ import { useState, useEffect } from 'react';
 export default (apiUrl) => {
     const [stats, setStats] = useState();
     const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const fetchData = async () => {
         try {
             setIsError(false);
+            setIsLoading(true);
+
             const results = await fetch(`${apiUrl}`);
             console.log(`Fetching data for: ${apiUrl}`);
             setStats(results.json());
         } catch (err) {
             setIsError(err)
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -21,6 +26,7 @@ export default (apiUrl) => {
 
     return {
         stats,
-        isError
+        isError,
+        isLoading
     };
 }
