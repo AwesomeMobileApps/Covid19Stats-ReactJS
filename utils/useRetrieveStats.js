f989ab3e9bb0788297838a75b056715164914869
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2024-09-23 17:47:18 +1000

    Enhance logic/order instruction + indentation cleanup

--- utils/useRetrieveStats.js
+++ utils/useRetrieveStats.js
@@ -1,32 +1,32 @@
 import { useState, useEffect } from 'react';
 
 export default (apiUrl) => {
-    const [stats, setStats] = useState();
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+  const [stats, setStats] = useState();
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const fetchData = async () => {
-        try {
-            setIsError(false);
-            setIsLoading(true);
+  const fetchData = async () => {
+    setIsError(false);
+    setIsLoading(true);
 
-            const results = await fetch(`${apiUrl}`);
-            console.log(`Fetching data for: ${apiUrl}`);
-            setStats(results.json());
-        } catch (err) {
-            setIsError(err)
-        } finally {
-            setIsLoading(false);
-        }
-    };
+    try {
+      const results = await fetch(`${apiUrl}`);
+      console.log(`Fetching data for: ${apiUrl}`);
+      setStats(await results.json());
+    } catch (err) {
+      setIsError(err)
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [apiUrl]);
+  useEffect(() => {
+    fetchData();
+  }, [apiUrl]);
 
-    return {
-        stats,
-        isError,
-        isLoading
-    };
+  return {
+    stats,
+    isError,
+    isLoading
+  };
 }
